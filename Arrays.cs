8daf5074bc72a3fc42f8646dc25110435896219f
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-02-24 10:18:52 +0100

    Create Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,19 @@
+using System;                               // npc
+
+namespace ArraysSUVs                        // organize code | a container for classes & other namespaces.
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] suvs = {"Audi Q3 Quattro Sport", "Toyota FJ Cruiser", "Honda HR-V", "Nissan Patrol", "Ford Explorer", "Land Rover Range Rover",
+      "Porsche Cayenne", "Mazda CX-9", "Mitsubishi Pajero", "Jeep Grand Cherokee" };
+      string[] suvs2 = new string[4];
+      Console.WriteLine("List of SUVs");    // ex initial npc
+      Console.WriteLine(cars.Length);
+      Console.WriteLine(suvs[0]);
+      
+    }
+  }
+}
+
