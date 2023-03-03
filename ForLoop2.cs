56602e19d678c83d374176c57c91eddcced02059
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-03-03 11:54:38 +0100

    Create ForLoop2.cs

---
+++ ForLoop2.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbyX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 10; x > 0; x-=2)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
+
+
+/*
+Outputs:
+10
+8
+6
+4
+2
+*/
