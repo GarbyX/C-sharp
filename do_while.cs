3c7eb75c34b4c41bb637cb83763bd93b40a47a59
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-03-04 12:35:52 +0100

    Create do_while.cs

---
+++ do_while.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            do {
+                Console.WriteLine(a);
+                a++;
+            } while(a < 5);
+        }
+    }
+}
+
+/*
+Outputs:
+0
+1
+2
+3
+4
+*/
